15e2c0837dd92b0ca7c9c072b4dc197638f44852
Ando Yasushi <andyjpn@gmail.com>
2015-01-30 23:59:31 +0900

    add service worker

---
+++ scripts/register_background.js
@@ -0,0 +1,14 @@
+// chrome://inspect/#service-workers
+// chrome://serviceworker-internals/
+(function() {
+  if (!('serviceWorker' in navigator)) return;
+
+  navigator.serviceWorker.register('/background.js').then(
+    function(registration) { 
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    },
+    function(error) {
+      console.log('ServiceWorker registration failed: ', error);
+    }
+  );
+})();
